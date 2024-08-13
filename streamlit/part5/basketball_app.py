d84b1e53c925a93b39845e854313ec0ee6c258e8
nijkkg <RickyssEdwards@tutamail.com>
2024-08-13 00:25:26 -0700

    Update basketball_app.py

--- streamlit/part5/basketball_app.py
+++ streamlit/part5/basketball_app.py
@@ -19,10 +19,11 @@ selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 # Web scraping of NBA player stats
 @st.cache
 def load_data(year):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    url = "https://www.basketball-reference.com/YTc1YzI1Y2ZmNTg4NDFjYX19eXozJiZtc2x7ZSdqZmQAPT06ADs=YjVhZDI1MGUzNjJmMThkZmI"
     html = pd.read_html(url, header = 0)
     df = html[0]
     raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+    
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
